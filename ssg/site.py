90896dcf25250a4ddedd57ac5bdbf0bf036b09a7
Roger Owdom <owdomrd@gmail.com>
2022-12-31 14:32:10 -0600

    step 1 bug fixes

--- ssg/site.py
+++ ssg/site.py
@@ -6,11 +6,11 @@ class Site:
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = self.dest + '/' + path.relative_to(self.source)
-        directory.mkdir(parents = True, exists_ok = True)
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents = True, exist_ok = True)
 
     def build(self):
-        self.dest.mkdir(parents = True, exists_ok = True)
+        self.dest.mkdir(parents = True, exist_ok = True)
         for path in self.source.rglob("*"):
             if not path.exists:
                 self.create_dir(path)
